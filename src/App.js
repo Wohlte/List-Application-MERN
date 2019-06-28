6662001dc4ebc14553d659ae0c009db6b512fb80
Albert Park <albert.park@hotmail.com>
2019-06-28 14:06:45 -0700

    Implemented front-end with React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import CreateTodo from "./components/create-todo-component";
+import EditTodo from "./components/edit-todo-component";
+import TodosList from "./components/todos-list-component";
+
+import logo from "./logo.svg"
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>MERN-Stack Todo App</h2>
-      </div>
+      <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg light bg-light">
+            <a className="navbar-brand" href="https://codingthesmartway.com">
+              <img src={logo} width="30" height="30" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div>
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+
+      </Router>
     );
   }
 }
